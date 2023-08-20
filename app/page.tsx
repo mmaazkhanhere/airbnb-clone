52d8a125f766230eb5b404cd9202cb41d30d9eeb
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-08-20 15:33:34 +0500

    project started and header created

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Header from '@/components/Header'
+import React from 'react'
+
+type Props = {}
+
+const Home = (props: Props) => {
+  return (
+    <div className=''>
+      <Header />
+    </div>
+  )
+}
+
+export default Home
